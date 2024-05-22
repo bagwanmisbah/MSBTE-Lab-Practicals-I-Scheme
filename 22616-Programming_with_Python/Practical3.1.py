52c6c07f1b1161f7db632f5e91bc9b40c012fac9
bagwanmisbah <162052331+bagwanmisbah@users.noreply.github.com>
2024-05-23 02:39:28 +0530

    Create Practical3.1.py

---
+++ 22616-Programming_with_Python/Practical3.1.py
@@ -0,0 +1,7 @@
+'''PRACTICAL NO. 3:  
+WRITE SIMPLE PYTHON PROGRAM USING OPERATORS: 
+ARITHMETIC OPERATORS, LOGICAL OPERATORS, BITWISE OPERATORS'''
+#Write a program to convert U.S. dollars to Indian rupees.
+d=float(input("Enter dollars")
+r=d*76.76
+print('f'{d} Dollars into Indian Rupees {r}')
