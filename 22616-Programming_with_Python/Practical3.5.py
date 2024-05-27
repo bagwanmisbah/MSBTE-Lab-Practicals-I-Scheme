d0a1acf6a2b797ad48e4d215f960a044c25b5af2
Misbah Bagwan <162052331+bagwanmisbah@users.noreply.github.com>
2024-05-27 15:50:01 +0530

    Create Practical3.5.py

---
+++ 22616-Programming_with_Python/Practical3.5.py
@@ -0,0 +1,5 @@
+# . WRITE A PROGRAM TO CALCULATE AREA AND PERIMETER OF THE SQUARE 
+side=int(input("Enter length of side: "))
+per=4*side
+area=side*side
+print(f'Perimeter : {per} \nArea : {area}')
