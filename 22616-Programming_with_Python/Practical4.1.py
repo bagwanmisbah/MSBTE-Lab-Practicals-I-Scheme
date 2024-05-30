bb0a0c86fc463acb8420bbb56ff660356584023c
Misbah Bagwan <162052331+bagwanmisbah@users.noreply.github.com>
2024-05-30 22:36:15 +0530

    Update Practical4.1.py

--- 22616-Programming_with_Python/Practical4.1.py
+++ 22616-Programming_with_Python/Practical4.1.py
@@ -1,1 +1,7 @@
  #1. WRITE A PROGRAM TO CHECK WHETHER A NUMBER IS EVEN OR ODD 
+num=int(input("Enter any number: "))
+if num%2==0:
+   print(f'{num} is an even number')
+else:
+   print(f'{num} is an odd number')
+   
