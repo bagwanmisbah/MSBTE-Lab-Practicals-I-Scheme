0d1fbc9e1a33cfe9d4b94aa61983d51aeff2ee08
Misbah Bagwan <162052331+bagwanmisbah@users.noreply.github.com>
2024-06-02 20:28:20 +0530

    Update Practical4.5.py

--- 22616-Programming_with_Python/Practical4.5.py
+++ 22616-Programming_with_Python/Practical4.5.py
@@ -1,1 +1,9 @@
-#
+#WRITE A PROGRAM TO CHECK IF A NUMBER IS POSITIVE, NEGATIVE OR ZERO 
+num=int(input("Enter a number:"))
+if num>0:
+  print(f'{num} is a positive number')
+elif num<0:
+  print(f'{num} is a negative number')
+else:
+  print("Zero")
+  
