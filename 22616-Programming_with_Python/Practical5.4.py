a8ff9cd2eec52130e65ae970d8da73b5772acbe5
Misbah Bagwan <162052331+bagwanmisbah@users.noreply.github.com>
2024-06-25 19:50:21 +0530

    Update Practical5.4.py

--- 22616-Programming_with_Python/Practical5.4.py
+++ 22616-Programming_with_Python/Practical5.4.py
@@ -1,12 +1,15 @@
 #WRITE A PYTHON PROGRAM TO PRINT FIBONACCI SERIES.
 # 0  1  1  2  3  5  8  13 
 
-fn=0,sn=1,tn
+fn=0
+sn=1
 num=int(input("Enter the position upto which Fibonacci Series is to be printed: "))
-i=num
+i=0
 while(i<num):
-  print(f'{fn} {sn}')
+  print(f'{fn}')
+  tn=fn+sn
   fn=sn
-  sn=fn+sn
+  sn=tn
+  i=i+1
   
   
