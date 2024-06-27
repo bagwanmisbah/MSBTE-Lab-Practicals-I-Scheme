99f68e43ca2aac338a43ff5afd1f6623b67aa64e
Misbah Bagwan <162052331+bagwanmisbah@users.noreply.github.com>
2024-06-27 21:51:24 +0530

    Create Practical5.6.py

---
+++ 22616-Programming_with_Python/Practical5.6.py
@@ -0,0 +1,9 @@
+#WRITE A PYTHON PROGRAM TO REVERSE A GIVEN NUMBER
+num=int(input("Enter a number  to reverse it: "))
+while num>0:
+  rem=num%10
+  new=rem*10
+  num=num/10
+print(rem)
+print(num0
+  
