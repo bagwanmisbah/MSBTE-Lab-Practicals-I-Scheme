c8034d5ddab1095d36ba23aeec1e534ee909539e
Misbah Bagwan <162052331+bagwanmisbah@users.noreply.github.com>
2024-08-20 21:09:57 +0530

    Add files via upload

---
+++ 22617_Mobile_Application_Development/MisbahAbsoluteLayoutExample/app/src/main/java/com/example/misbahabsolutelayoutexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.misbahabsolutelayoutexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
